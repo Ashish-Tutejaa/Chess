04c4a59a3b49f0537d22d6c39d996e27d487809e
Ashish <ashish.tutejaa@gmail.com>
2021-02-24 22:46:50 +0700

    takes takes takes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { type } from "os";
 import React, { useEffect, useRef, useState } from "react";
 import { Children } from "react";
+import { createNoSubstitutionTemplateLiteral } from "typescript";
 import "./App.css";
-import { Pieces, make_array } from "./moves";
+import { Pieces, make_array, chessPiece } from "./moves";
 import { useAllRefs } from "./useAllRefs";
 
 interface pos {
   childPosY: number;
   childPosX: number;
+  loc: string;
 }
 
 type newMouseEventOnDiv = React.MouseEvent<HTMLDivElement, MouseEvent> & pos;
@@ -26,21 +29,28 @@ interface toTd {
 interface toPiece {
   thisIsARef: React.MutableRefObject<HTMLDivElement | null>;
   name: string;
+  pieceInfo: chessPiece;
 }
 
-const Piece: (props: toPiece) => JSX.Element = ({ thisIsARef, name }) => {
+const Piece: (props: toPiece) => JSX.Element = ({
+  thisIsARef,
+  name,
+  pieceInfo,
+}) => {
   const clickedRef = useRef<true | false>(false);
+  const src = `${pieceInfo.side}${name[0]}`.toLowerCase();
 
   return (
     <div
+      style={{
+        backgroundImage: `url("${src}.png")`,
+      }}
       ref={thisIsARef}
       onMouseMove={(e) => {
-        e.stopPropagation();
+        // e.stopPropagation();
       }}
       className={`piece ${name}`}
-    >
-      {name}
-    </div>
+    ></div>
   );
 };
 
@@ -56,13 +66,12 @@ const Td: (props: toTd) => JSX.Element = ({
     <td
       id={`${row}${col}`}
       onMouseDown={(e: newMouseEventOnCell) => {
+        e.loc = e.currentTarget.id;
         e.childPosX = e.currentTarget.getBoundingClientRect().left;
         e.childPosY = e.currentTarget.getBoundingClientRect().top;
         return;
       }}
       onMouseMove={(e: newMouseEventOnCell) => {
-        e.childPosX = e.currentTarget.getBoundingClientRect().left;
-        e.childPosY = e.currentTarget.getBoundingClientRect().top;
         return;
       }}
       className={`${i !== j ? "oddCol" : "evenCol"}`}
@@ -80,54 +89,141 @@ for (let i in Pieces) {
   let [x, y] = Pieces[i].cur_coords();
   initBoard[7 - y][x] = Pieces[i].name;
 }
-console.log(initBoard);
 
 const ChessBoard = () => {
   const [board, setBoard] = useState<Array<Array<string>>>(initBoard);
+  const [turn, setTurn] = useState<string>("W");
   const Refs = useAllRefs();
-  const parentNameRef = useRef<string | null>(null);
+  const parentInfoRef = useRef<null | {
+    x: number;
+    y: number;
+    name: string;
+    loc: string;
+    dropLoc: string[];
+  }>(null);
   const clickedRef = useRef<false | true>(false);
-  const parentY = useRef<number | null>(null);
-  const parentX = useRef<number | null>(null);
   let a = [0, 1, 2, 3, 4, 5, 6, 7];
   return (
     <div
       onMouseDown={(e: newMouseEventOnDiv) => {
-        if (
-          Array.from((e.target as HTMLDivElement).classList).includes(
-            "piece"
-          ) === false
-        )
-          return;
-        const targetRef =
-          Refs[Array.from((e.target as HTMLDivElement).classList)[1]];
+        const list = Array.from((e.target as HTMLDivElement).classList);
+
+        if (!list.includes("piece")) return;
+        const name = list[1];
+        if (Pieces[name].side !== turn) return;
+        const targetRef = Refs[name];
+
         if (targetRef.current !== null) {
-          targetRef.current.style.top = `${e.clientY - 15 - e.childPosY}px`;
-          targetRef.current.style.left = `${e.clientX - 15 - e.childPosX}px`;
+          targetRef.current.style.zIndex = "100";
+          targetRef.current.style.transform = `translateX(${
+            e.clientX - 15 - e.childPosX
+          }px) translateY(${e.clientY - 15 - e.childPosY}px)`;
+
+          let dropLocations = (Pieces[name] as any).finder(
+            board,
+            Number(e.loc[0]),
+            Number(e.loc[1])
+          );
+
+          let processed: string[] = dropLocations.reduce(
+            (acc: [], cur: number[][], i: number) => {
+              return [
+                ...acc,
+                ...cur.map((ele: number[]) => {
+                  return ele.join("");
+                }),
+              ];
+            },
+            []
+          );
+
           clickedRef.current = true;
-          parentNameRef.current = Array.from(
-            (e.target as HTMLDivElement).classList
-          )[1];
-          parentY.current = e.childPosY;
-          parentX.current = e.childPosX;
+          parentInfoRef.current = {
+            x: e.childPosX,
+            y: e.childPosY,
+            loc: e.loc,
+            name: Array.from((e.target as HTMLDivElement).classList)[1],
+            dropLoc: processed,
+          };
+
+          for (let j of processed) {
+            let x = document.getElementById(j);
+            if (x !== null) {
+              x.classList.add("selCol");
+            }
+          }
         }
       }}
       onMouseUp={(e) => {
-        console.log(document.elementsFromPoint(e.clientX, e.clientY));
+        if (clickedRef.current === false || parentInfoRef.current === null)
+          return;
+
+        const { name, dropLoc } = parentInfoRef.current;
+        const targetRef = Refs[name];
+        let dropTarget: any = null;
+        Array.from(document.elementsFromPoint(e.clientX, e.clientY)).forEach(
+          (ele) => {
+            if (dropTarget !== null) return;
+            if (ele.nodeName === "TD") dropTarget = ele;
+          }
+        );
+
+        let skip = false;
+        if (dropTarget !== null && dropLoc.includes(dropTarget.id)) {
+          let x = Number(dropTarget.id[1]),
+            y = Number(dropTarget.id[0]);
+          if (board[x][y][0] === "K") {
+            if (board[x][y][1] === "1") alert("White wins");
+            else alert("Black wins");
+            setBoard(initBoard);
+            setTurn("W");
+            skip = true;
+          }
+
+          if (!skip) {
+            let tboard = JSON.parse(JSON.stringify(board));
+            for (let i = 0; i < tboard.length; i++) {
+              for (let j = 0; j < tboard[0].length; j++) {
+                if (tboard[i][j] === name) {
+                  tboard[i][j] = "";
+                  break;
+                }
+              }
+            }
+            tboard[Number(dropTarget.id[1])][Number(dropTarget.id[0])] = name;
+            if (name[0] === "P") {
+              if ("first_move" in Pieces[name])
+                (Pieces[name] as any).first_move = false;
+            }
+            setBoard(tboard);
+            setTurn((p) => (p === "W" ? "B" : "W"));
+          }
+        } else if (targetRef.current !== null) {
+          targetRef.current.style.zIndex = "10";
+          targetRef.current.style.transform = ``;
+        }
+
+        for (let j of dropLoc) {
+          let x = document.getElementById(j);
+          if (x !== null) {
+            x.classList.remove("selCol");
+          }
+        }
+
         clickedRef.current = false;
-        parentX.current = null;
-        parentY.current = null;
+        parentInfoRef.current = null;
       }}
       onMouseMove={(e: newMouseEventOnDiv) => {
-        if (clickedRef.current === false) return;
-        const targetRef = Refs[parentNameRef.current as string];
+        if (clickedRef.current === false || parentInfoRef.current === null)
+          return;
+
+        const { name: pieceName, loc: pieceLoc, x, y } = parentInfoRef.current;
+        const targetRef = Refs[pieceName];
+
         if (targetRef.current !== null) {
-          targetRef.current.style.top = `${
-            e.clientY - 15 - (parentY.current as number)
-          }px`;
-          targetRef.current.style.left = `${
-            e.clientX - 15 - (parentX.current as number)
-          }px`;
+          targetRef.current.style.transform = `translateX(${
+            e.clientX - 15 - x
+          }px) translateY(${e.clientY - 15 - y}px)`;
         }
       }}
       className="board"
@@ -141,6 +237,7 @@ const ChessBoard = () => {
                 if (board[i][j] !== "")
                   pieceRN = (
                     <Piece
+                      pieceInfo={Pieces[board[i][j]]}
                       name={`${board[i][j]}`}
                       thisIsARef={Refs[`${board[i][j]}`]}
                     />
@@ -148,8 +245,8 @@ const ChessBoard = () => {
 
                 return (
                   <Td
-                    row={i}
-                    col={j}
+                    row={j}
+                    col={i}
                     piece={i === 0 && j === 0}
                     i={i % 2 === 1}
                     j={j % 2 === 0}
--- src/moves.ts
+++ src/moves.ts
@@ -1,14 +1,34 @@
+import { dir } from "console";
+import { Duplex } from "stream";
+import { directions } from "./directions";
+
 interface origin {
   coords: { [prop: string]: Array<number> };
   castle_left: boolean;
   castle_right: boolean;
-  finder: () => void;
+  finder: (
+    this: any,
+    board: Array<Array<string>>,
+    x: number,
+    y: number
+  ) => (number[][] | null)[] | null;
 }
-interface chessPiece {
+export interface chessPiece {
   side: string;
   name: string;
   cur_coords: () => [number, number];
+  first_piece?: boolean;
 }
+
+export const check: (
+  x: number,
+  y: number,
+  lx: number,
+  ly: number
+) => true | false = (x, y, lx, ly) => {
+  if (x < 0 || y < 0 || x >= lx || y >= ly) return false;
+  return true;
+};
 
 export const make_array: (x: number) => Array<string> = (x) => {
   let t = [];
@@ -37,12 +57,126 @@ const build_coords: () => { [prop: string]: Array<number> } = () => {
   return res;
 };
 
+const gen_direc_wrapper = (
+  direction: number[][][],
+  board: Array<Array<string>>,
+  x0: number,
+  y0: number,
+  side: string
+) => {
+  let res = [];
+  for (let i of direction) {
+    res.push(gen_direc(i, board, x0, y0, side));
+  }
+
+  return res;
+};
+
+const side_finder = (name: string) => {
+  if (black_pieces.includes(name)) return "B";
+  if (white_pieces.includes(name)) return "W";
+  if (name.length === 3 || Number(name[0]) > 8) return "B";
+  return "W";
+};
+
+const gen_direc = (
+  direction: number[][],
+  board: Array<Array<string>>,
+  x0: number,
+  y0: number,
+  side: string
+) => {
+  let res = [];
+  let lx = board[0].length;
+  let ly = board.length;
+  for (let i = 0; i < direction[0].length; i++) {
+    let nx = x0 + direction[0][i],
+      ny = y0 + direction[1][i];
+    if (check(nx, ny, lx, ly)) {
+      if (board[ny][nx].length !== 0) {
+        let targSide = side_finder(board[ny][nx]);
+
+        if (side === targSide) {
+          if (board[y0][x0][0] === "H" || board[y0][x0][0] === "K") continue;
+          break;
+        } else {
+          res.push([nx, ny]);
+          if (board[y0][x0][0] === "H" || board[y0][x0][0] === "K") continue;
+          break;
+        }
+      }
+      res.push([nx, ny]);
+    }
+  }
+  return res;
+};
+
 const origin: origin = {
   coords: build_coords(),
   castle_left: false,
   castle_right: false,
-  finder() {
-    return;
+  finder(board, x, y) {
+    let lx = board[0].length,
+      ly = board.length;
+    if (this.name[0] === "R") {
+      return gen_direc_wrapper(
+        [directions.l, directions.r, directions.u, directions.d],
+        board,
+        x,
+        y,
+        this.side
+      );
+    } else if (this.name[0] === "B") {
+      return gen_direc_wrapper(
+        [directions.ul, directions.ur, directions.dl, directions.dr],
+        board,
+        x,
+        y,
+        this.side
+      );
+    } else if (this.name[0] === "H") {
+      return gen_direc_wrapper([directions.h], board, x, y, this.side);
+    } else if (this.name[0] === "K") {
+      return gen_direc_wrapper([directions.k], board, x, y, this.side);
+    } else if (this.name[0] === "Q") {
+      const { l, r, u, d, ul, ur, dl, dr } = directions;
+      return gen_direc_wrapper(
+        [l, r, u, d, ul, ur, dl, dr],
+        board,
+        x,
+        y,
+        this.side
+      );
+    } else if (this.name[0] === "P") {
+      let res = [];
+      let sign = -1;
+      if (this.side === "B") sign += 2;
+
+      if (this.first_move === true) {
+        if (
+          check(x, y + 2 * sign, lx, ly) &&
+          board[y + 2 * sign][x].length === 0
+        ) {
+          res.push([x, y + 2 * sign]);
+        }
+      }
+      if (check(x + 1, y + 1 * sign, lx, ly)) {
+        if (board[y + 1 * sign][x + 1].length !== 0) {
+          let ts = side_finder(board[y + 1 * sign][x + 1]);
+          if (ts !== this.side) res.push([x + 1, y + 1 * sign]);
+        }
+      }
+      if (check(x - 1, y + 1 * sign, lx, ly)) {
+        if (board[y + 1 * sign][x - 1].length !== 0) {
+          let ts = side_finder(board[y + 1 * sign][x - 1]);
+          if (ts !== this.side) res.push([x - 1, y + 1 * sign]);
+        }
+      }
+      if (check(x, y + 1 * sign, lx, ly) && board[y + 1 * sign][x].length === 0)
+        res.push([x, y + 1 * sign]);
+      return [res];
+    }
+    return null;
   },
 };
 
@@ -52,6 +186,9 @@ function PieceCreator(this: any, name: string, side: string) {
   this.cur_coords = () => {
     return this.coords[this.name];
   };
+  if (name[0] === "P") {
+    this.first_move = true;
+  }
 }
 PieceCreator.prototype = origin;
 
